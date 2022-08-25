65fe45ef5292b091fed97a17cb774eb59032d0c9
Vishal <vishalwankhede44@gmail.com>
2022-08-25 23:25:35 +0530

    Test file updated

--- src/test/java/com/cognizant/ProcessPension/ProcessPensionApplicationTests.java
+++ src/test/java/com/cognizant/ProcessPension/ProcessPensionApplicationTests.java
@@ -1,10 +1,20 @@
 package com.cognizant.ProcessPension;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProcessPensionApplicationTests {
+import com.cognizant.ProcessPension.controllerTest.ProcessPensionControllerTest;
+import com.cognizant.ProcessPension.modelTest.ProcessPensionModelTest;
+import com.cognizant.ProcessPension.serviceTest.ProcessPensionServiceTest;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ProcessPensionModelTest.class,ProcessPensionControllerTest.class,ProcessPensionServiceTest.class})
+
+public class ProcessPensionApplicationTests {
 
 	
 
